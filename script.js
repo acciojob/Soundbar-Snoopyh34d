eeed5db4135adbdeab50f32ee3be8c74bb5e73ea
Prabhat Ayush <60286121+Snoopyh34d@users.noreply.github.com>
2024-01-26 22:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let audio;
+
+function playSound(soundFileName) {
+    stopAudio(); // Stop any currently playing audio
+
+    audio = new Audio(`sounds/${soundFileName}`);
+    audio.play();
+}
+
+function stopAudio() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
